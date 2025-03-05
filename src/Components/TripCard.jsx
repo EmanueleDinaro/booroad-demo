257016119e084db1510cc8b918e99eee3ddda4c0
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-03-05 14:08:50 +0100

    feat: add style card, bg & accordion

--- src/Components/TripCard.jsx
+++ src/Components/TripCard.jsx
@@ -7,31 +7,46 @@ export default function TripCard({
   endDate,
   groupLeader,
   link,
+  participants,
 }) {
   return (
     <Link to={link} className="block">
-      <div className="bg-white shadow-lg rounded-2xl p-5 w-80 border border-gray-200 transform transition duration-300 hover:scale-105 mx-auto">
-        <h2 className="text-xl font-semibold text-gray-800 mb-2">
-          {destination}
-        </h2>
+      <div className="bg-white shadow-lg rounded-2xl w-75 border border-gray-200 transform transition duration-300 hover:scale-105 mx-auto">
+        <div className>
+          <h2 className=" bg-cyan-700 rounded-t-2xl text-xl font-semibold text-gray-50 p-2 mb-0 z-10 relative shadow-[0px_5px_10px_rgba(0,0,0,0.4)]">
+            {destination}
+          </h2>
+        </div>
         <img
-          className="rounded-xl h-50 w-80 object-cover object-center"
+          className="h-50 w-80 object-cover object-center rounded-b-2xl"
           src={destination_image}
           alt={destination}
         />
-        <div className="text-sm text-gray-700">
-          <p className="font-semibold text-gray-600">
+        <div className="text-sm text-gray-700 space-y-2 p-4">
+          <p className="font-semibold text-gray-600 flex items-center gap-2">
+            <i className="fa-solid fa-calendar-alt text-gray-500"></i>
             Data Inizio:{" "}
             <span className="font-normal text-gray-800">{startDate}</span>
           </p>
-          <p className="font-semibold text-gray-600">
+          <p className="font-semibold text-gray-600 flex items-center gap-2">
+            <i className="fa-solid fa-calendar-alt text-gray-500"></i>
             Data Fine:{" "}
             <span className="font-normal text-gray-800">{endDate}</span>
           </p>
-          <p className="font-semibold text-gray-600">
+          <p className="font-semibold text-gray-600 flex items-center gap-2">
+            <i className="fa-solid fa-crown text-yellow-500"></i>
             Capogruppo:{" "}
             <span className="font-normal text-gray-800">
-              {groupLeader?.firstName} {groupLeader?.lastName}
+              {groupLeader
+                ? `${groupLeader.firstName} ${groupLeader.lastName}`
+                : "Nessuno"}
+            </span>
+          </p>
+          <p className="font-semibold text-gray-600 flex items-center gap-2">
+            <i className="fa-solid fa-users text-gray-500"></i>
+            Partecipanti:{" "}
+            <span className="font-normal text-gray-800">
+              {participants?.length}
             </span>
           </p>
         </div>
